ed088092f0177a978b95c4a865e8826ebfe94676
Romain Fallet <romain.fallet@outlook.com>
2017-05-02 18:45:09 +0200

    Update webpack.envars.js

--- webpack.envars.js
+++ webpack.envars.js
@@ -1,19 +1,25 @@
 // Set the `ENV` global variable to be used in the app.
 var path = require('path');
 var webpack = require('webpack');
+var config;
 
-var projectRootDir = process.env.IONIC_ROOT_DIR;
-var appScriptsDir = process.env.IONIC_APP_SCRIPTS_DIR;
+if (process.env.IONIC_APP_SCRIPTS_DIR) {    
+    var config = require(path.join(process.env.IONIC_APP_SCRIPTS_DIR, 'config', 'webpack.config.js'));
+}
+else {
+    var config = require(path.join(__dirname, 'test-config', 'webpack.test.js'))
+}
 
-var config = require(path.join(appScriptsDir, 'config', 'webpack.config.js'));
 
 var env = process.env.IONIC_ENV || 'dev';
 var envVars;
+
 try {
-  envVars = require(path.join(projectRootDir, 'env', env + '.json'));
+  envVars = require(path.join(__dirname, 'env', env + '.json'));
 } catch(e) {
   envVars = {};
 }
+
 Object.keys(envVars).forEach(function(key) {
  envVars[key] =  JSON.stringify(envVars[key]).replace(/"/g, '\"');
 });
