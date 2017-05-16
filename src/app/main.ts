af73ac5b5bada865c7df80c0878a9fa0e5d60bf4
Numëe <comptes@numee.fr>
2017-05-16 12:52:38 +0200

    clean repo

--- src/app/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-import { enableProdMode } from '@angular/core';
-declare const ENV;
-
-if (ENV.PRODUCTION) { enableProdMode(); }
-
-platformBrowserDynamic().bootstrapModule(AppModule);
